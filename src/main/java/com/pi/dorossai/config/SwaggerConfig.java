9aee710a93d37b81d89df8187963442eb0fe2dd5
AbderrahimeEl <elmoutaouakil12345@gmail.com>
2025-05-04 09:23:17 +0100

    add FastAPI service configuration and Swagger/OpenAPI settings

---
+++ src/main/java/com/pi/dorossai/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.pi.dorossai.config;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SecurityScheme(
+        name = "BearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("My API").version("v1"))
+                .addSecurityItem(new SecurityRequirement().addList("BearerAuth")); // 👈 This makes it global
+    }
+}
